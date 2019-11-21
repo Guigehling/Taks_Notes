83b2c3f7b775cd0311adb55befcbcde1a66bdcb7
Guilherme Gehling <guilherme.gehling@calcard.com.br>
2019-11-20 23:17:00 -0300

    Firt Commit

---
+++ src/screens/Agenda.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyle from '../commonStyles';
+
+export default class Agenda extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>
+              {moment()
+                .locale('pt-br')
+                .format('ddd, D [de] MMMM')}
+            </Text>
+          </View>
+        </ImageBackground>
+
+        <View style={styles.taskContainer}>
+          <Text>Task 01</Text>
+          <Text>Task 02</Text>
+          <Text>Task 03</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyle.fontFamily,
+    color: commonStyle.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyle.fontFamily,
+    color: commonStyle.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+  taskContainer: {
+    flex: 7,
+  },
+});
