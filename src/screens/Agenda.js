2789ca6ff1d3db387112aa58975d6aedf3a50bd3
Guilherme Gehling <guigehling@gmail.com>
2020-01-19 16:45:25 -0200

    Check e uncheck tarefas

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -26,6 +26,27 @@ export default class Agenda extends Component {
     ],
   };
 
+  toggleTask = id => {
+    //Percorrendo lista com Map
+    const tasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task = {...task};
+        task.doneAt = task.doneAt ? null : new Date();
+      }
+      return task;
+    });
+
+    //Percorrendo lista com ForEach
+    // const tasks = [...this.state.tasks];
+    // tasks.forEach(task => {
+    //   if (task.id === id) {
+    //     task.doneAt = task.doneAt ? null : new Date();
+    //   }
+    // });
+
+    this.setState({tasks});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -44,7 +65,9 @@ export default class Agenda extends Component {
           <FlatList
             data={this.state.tasks}
             keyExtractor={item => `${item.id}`}
-            renderItem={({item}) => <Task {...item} />}
+            renderItem={({item}) => (
+              <Task {...item} toggleTask={this.toggleTask} />
+            )}
           />
         </View>
       </View>
